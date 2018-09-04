ec7007be328c588c144262da723dd1eae93dba01
Jason Axelson <jason@hobnob.io>
2018-09-04 11:50:19 -0700

    Try debugger

--- lib/nerves_kiosk.ex
+++ lib/nerves_kiosk.ex
@@ -1,18 +1,8 @@
 defmodule NervesKiosk do
-  @moduledoc """
-  Documentation for NervesKiosk.
-  """
+  require Logger
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> NervesKiosk.hello
-      :world
-
-  """
   def hello do
-    :world
+    Logger.debug("Hello")
+    :hworld
   end
 end
